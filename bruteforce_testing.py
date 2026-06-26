2f528a6f7031dddafcf30ae5a468fc0b8d6190e7
vrushti0908 <vrushti0908@gmail.com>
2026-06-26 20:10:38 +0530

    Delete bruteforce_testing.py

--- bruteforce_testing.py
+++
@@ -1,5 +0,0 @@
-from playbooks.bruteforce import handle_bruteforce
-
-print(handle_bruteforce({"risk_score": 90}))  # Block IP
-print(handle_bruteforce({"risk_score": 60}))  # Create Case
-print(handle_bruteforce({"risk_score": 30}))  # Log Event
