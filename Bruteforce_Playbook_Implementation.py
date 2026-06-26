8ab3ca168d8bab2395aa502a1e38a74a7b694547
vrushti0908 <vrushti0908@gmail.com>
2026-06-26 20:08:30 +0530

    Delete Bruteforce_Playbook_Implementation.py

--- Bruteforce_Playbook_Implementation.py
+++
@@ -1,9 +0,0 @@
-def handle_bruteforce(alert):
-    risk = alert.get("risk_score", 0)
-
-    if risk > 80:
-      return "Block IP"
-    elif risk > 50:
-        return "Create Case"
-    else:
-        return "Log Event"
